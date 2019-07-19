6f396cea7a325c10796ed7d5bdf1656e33de26b1
Michael Ihde <mihde@spectric.com>
2019-07-18 22:09:09 -0400

    Updates

---
+++ precache-manifest.e79930abe80cdd6d2830cca9e1c29a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a27675a382bd8260dc4fddf18e725f",
+    "url": "/sigplot-tutorial/index.html"
+  },
+  {
+    "revision": "434a156056f36f60ec44",
+    "url": "/sigplot-tutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sigplot-tutorial/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "434a156056f36f60ec44",
+    "url": "/sigplot-tutorial/static/js/main.a57f7c83.chunk.js"
+  },
+  {
+    "revision": "d3b5554e384f769e30ad",
+    "url": "/sigplot-tutorial/static/js/runtime~main.9883d748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sigplot-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
